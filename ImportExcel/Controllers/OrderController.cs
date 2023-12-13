ef133cdb6cfcd09dfc5d8ff5e28ebe07675ea764
Rehman Gull <experts.web2@gmail.com>
2023-12-13 22:27:40 +0500

    Create Model and Controller

---
+++ ImportExcel/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImportExcel.Controllers
+{
+	public class OrderController : Controller
+	{
+		public IActionResult GetData()
+		{
+			List<Order> orders = new List<Order>()
+			{
+			 new Order(){Name = "Clarifying Graphene Sheet Mask",
+				Image="https://vouponlive.blob.core.windows.net/products/216/images/small_01_6cd6079e-5bf5-4965-b488-f31e5e3624c3.jpeg",
+				OrderDate = Convert.ToDateTime("11/7/2023"),
+				Price =  20.00M,
+				Discount =  17.00M
+			 }
+			};
+            return View(orders);
+        }
+	}
+}
---
+++ ImportExcel/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace ImportExcel
+{
+	public class Order
+	{
+        public string? Image { get; set; }
+        public string? Name { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
